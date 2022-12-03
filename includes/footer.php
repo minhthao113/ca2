1c1421ddc121592c6d78055259ed7c33807b9d9b
minhthao113 <thaocmng@gmail.com>
2022-12-03 20:06:38 +0000

    edit footer

--- includes/footer.php
+++ includes/footer.php
@@ -1,5 +1,5 @@
 <footer>
-<p>&copy; <?php echo date("Y"); ?>My Assignment Management App, Inc.</p>
+<p>&copy; <?php echo date("Y"); ?> My Assignment Management App, Inc.</p>
 </footer>
 </div><!-- close div container -->
 </body>
