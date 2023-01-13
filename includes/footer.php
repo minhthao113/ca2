042c45509cc20085e103bbe2635ebce83965581a
minhthao113 <thaocmng@gmail.com>
2023-01-13 08:07:39 +0000

    Link Bootstrap in Footer and Header

--- includes/footer.php
+++ includes/footer.php
@@ -1,6 +1,8 @@
 <footer>
-<p>&copy; <?php echo date("Y"); ?> My Assignment Management App, Inc.</p>
+    <p>&copy; <?php echo date("Y"); ?> My Assignment Management App, Inc.</p>
 </footer>
 </div><!-- close div container -->
 </body>
+<!-- Boostrap JS CDN -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
 </html>
--- includes/header.php
+++ includes/header.php
@@ -1,7 +1,9 @@
 <!-- the head section -->
 <head>
 <title>My Assignment Management App</title>
-<link rel="stylesheet" type="text/css" href="css/mystyle.css">
+<!--<link rel="stylesheet" type="text/css" href="css/mystyle.css">-->
+<!-- Bootstrap CSS CDN -->
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
 </head>
 
 <!-- the body section -->
