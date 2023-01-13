d3f57f9bde2fef700a0c73bf053cbb1985f81291
minhthao113 <thaocmng@gmail.com>
2023-01-13 11:42:19 +0000

    delete home page

--- module_list.php
+++ module_list.php
@@ -46,7 +46,6 @@ include('includes/header.php');
         <input id="add_module_button" type="submit" value="Add">
     </form>
     <br>
-    <p><a href="index.php">Homepage</a></p>
 
     <?php
 include('includes/footer.php');
