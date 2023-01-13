1ce3b27df181947e4b406c4a634803a839b14265
minhthao113 <thaocmng@gmail.com>
2023-01-13 01:48:24 +0000

    delete image section in edit_assignment

--- edit_assignment.php
+++ edit_assignment.php
@@ -20,39 +20,6 @@ $error = "Invalid assignment data. Check all fields and try again.";
 include('error.php');
 } else {
 
-/**************************** Image upload ****************************/
-
-/*
-$imgFile = $_FILES['image']['name'];
-$tmp_dir = $_FILES['image']['tmp_name'];
-$imgSize = $_FILES['image']['size'];
-$original_image = filter_input(INPUT_POST, 'original_image');
-
-if ($imgFile) {
-$upload_dir = 'image_uploads/'; // upload directory	
-$imgExt = strtolower(pathinfo($imgFile, PATHINFO_EXTENSION)); // get image extension
-$valid_extensions = array('jpeg', 'jpg', 'png', 'gif'); // valid extensions
-$image = rand(1000, 1000000) . "." . $imgExt;
-if (in_array($imgExt, $valid_extensions)) {
-if ($imgSize < 5000000) {
-if (filter_input(INPUT_POST, 'original_image') !== "") {
-unlink($upload_dir . $original_image);                    
-}
-move_uploaded_file($tmp_dir, $upload_dir . $image);
-} else {
-$errMSG = "Sorry, your file is too large it should be less then 5MB";
-}
-} else {
-$errMSG = "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
-}
-} else {
-// if no image selected the old image remain as it is.
-$image = $original_image; // old image from database
-}
-
-*/
-/************************** End Image upload **************************/
-
 // If valid, update the assignment in the database
 require_once('database.php');
 
